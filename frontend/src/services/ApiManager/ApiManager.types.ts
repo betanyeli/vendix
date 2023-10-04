78f6f96ac13df150273d02fb8343534c1bc64831
betanyeli <betangelii@gmail.com>
2023-10-04 00:30:01 -0300

    feat: update api manager

--- frontend/src/services/ApiManager/ApiManager.types.ts
+++ frontend/src/services/ApiManager/ApiManager.types.ts
@@ -1,7 +1,22 @@
+/**
+ * Creates a new Product instance.
+ *
+ * @param {number} id - The product ID.
+ * @param {string} productName - The name of the product.
+ * @param {number} sellerId - The ID of the seller.
+ * @param {number} amountAvailable - The available amount.
+ * @param {number} cost - The cost of the product.
+ * @param {number} stock - The stock count.
+ * @param {Date} createdAt - The creation date.
+ */
+
 export interface Product {
   id: number;
-  name: string;
-  price: number;
+  productName: string;
+  sellerId: string;
+  amountAvailable: number;
+  cost: number;
+  createdAt: Date;
 }
 
 export interface ApiManagerHook {
